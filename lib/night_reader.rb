c33690a3c9b3500f1428b5eacefc3a2500094683
bschwartz10 <bschwartz92@gmail.com>
2017-02-07 22:28:26 -0700

    Complete methods/tests for night_reader.rb and night_reader_test.rb

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,5 +1,6 @@
 require './lib/file_reader'
 require './lib/letters'
+require 'pry'
 
 class NightReader
   attr_reader :file_reader
@@ -7,30 +8,41 @@ class NightReader
     @reader = FileReader.new
   end
 
-  # def decode_file_to_english
-  #   braille = @reader.open_the_file
-  #   char_count = braille.length
-  #   message_text = decode_input_to_english(braille)
+  def decode_file_to_english
+    braille = @reader.open_the_file
+    char_count = braille.length
+    message_text = characters_equal_english_letter(input)
 
-  #  File.write(ARGV[1], message_text)
-  #  p "Created '#{ARGV[1]}' containing #{char_count} characters"
+   File.write(ARGV[1], message_text)
+   p "Created '#{ARGV[1]}' containing #{char_count} characters"
 
-  # end
+  end
 
   def split_array_at_new_lines(input)
     input.split("\n")
-    
   end
 
-  def scan_input(input)
-    input.map do |line|
+  def scan_input(ready_to_scan)
+    scanned_input = ready_to_scan.map do |line|
       line.scan(/../)
     end
+    scanned_input
   end
   
-  # def zip_input(input)
-  #   scan_input[0].zip(scan_input[1], scan_input[2])
-  # end
+  def zip_input(scanned_input)
+    zipped_input = scanned_input[0].zip(scanned_input[1], scanned_input[2])
+    zipped_input
+  end
+  
+  def characters_equal_english_letter(zipped_input)
+    binding.pry
+    new_line = ""
+    zipped_input.each do |character|
+      english = LETTERS.key(character)
+      new_line << english
+    end
+    new_line
+  end
 
 end
 
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -12,25 +12,35 @@ class TestNightReader < Minitest::Test
 
   def test_scan_input
     night_reader = NightReader.new
-    result = night_reader.scan_input(["000.00", ".0.000", "..0..."])
+    ready_to_scan = night_reader.split_array_at_new_lines("000.00\n.0.000\n..0...")
+    result = night_reader.scan_input(ready_to_scan)
     assert_equal [["00","0.","00"], [".0",".0","00"], ["..","0.",".."]], result
   end
 
   def test_zip_input
-    skip
     night_reader = NightReader.new
-    result = night_reader.zip_input([["00","0.","00"], [".0",".0","00"], ["..","0.",".."]])
+    ready_to_scan = night_reader.split_array_at_new_lines("000.00\n.0.000\n..0...")
+    scanned_input = night_reader.scan_input(ready_to_scan)
+    result = night_reader.zip_input(scanned_input)
     assert_equal [["00", ".0", ".."], ["0.", ".0", "0."], ["00", "00", ".."]] , result
   end
 
   def test_characters_equal_english_letter
-    skip
-  end
-  # def test_transform_input_string_into_array
-  #   night_reader = NightReader.new
-  #   result = night_reader.transform_input_string_into_array("000.00.0.000..0...")
-  #   assert_equal ["000.00.0.000..0..."], result
-  # end
+    night_reader = NightReader.new
+    ready_to_scan = night_reader.split_array_at_new_lines("000.00\n.0.000\n..0...")
+    scanned_input = night_reader.scan_input(ready_to_scan)
+    zipped_input = night_reader.zip_input(scanned_input)
+    result_1 = night_reader.characters_equal_english_letter(zipped_input)
+    assert_equal "dog", result_1
+  
+    night_reader = NightReader.new
+    ready_to_scan = night_reader.split_array_at_new_lines("0.0.0..0.0..0.0.00\n0.00.00000..00...0\n..0...0.0...0.000.")
+    scanned_input = night_reader.scan_input(ready_to_scan)
+    zipped_input = night_reader.zip_input(scanned_input)
+    result_2 = night_reader.characters_equal_english_letter(zipped_input)
+    assert_equal "brett run", result_2
 
+
+  end
 
 end
